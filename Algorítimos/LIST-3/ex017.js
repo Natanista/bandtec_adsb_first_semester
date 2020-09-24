4076d612861795b8b34ce716714a113b4db89d5f
natanista <gdaynatan@gmail.com>
2020-09-24 17:15:02 -0300

    aula frizza - JS

---
+++ Algorítimos/LIST-3/ex017.js
@@ -0,0 +1,12 @@
+function acabou() {
+    img_pizza.src = 'img/inteira.jpeg';
+}
+function umMinuto() {
+    img_pizza.src = 'img/5minutos.jpeg';
+}
+function cincoMinutos() {
+    img_pizza.src = 'img/metade.jpeg';
+}
+function quinzeMinutos() {
+    img_pizza.src = 'img/resto.jpeg';
+}
