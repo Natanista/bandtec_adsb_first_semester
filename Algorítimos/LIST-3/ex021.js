d098ae9345784bcd9adfe8f5765b1e7bc2fe9991
natanista <gdaynatan@gmail.com>
2020-09-25 19:12:40 -0300

    exercícios algorítimos prof Frizza

---
+++ Algorítimos/LIST-3/ex021.js
@@ -0,0 +1,12 @@
+function vermelho() {
+    resultado.innerHTML = `<span style="color:white; background-color:red;">Pare! Aguarde o verde!</span>`;
+}
+
+function amarelo() {
+    resultado.innerHTML =
+        `<span style="color:black; background-color:orange;">Só siga se estiver perto e rápido.</span>`;
+}
+
+function verde() {
+    resultado.innerHTML = `<span style="color:white; background-color:green;">Siga</span> :)`;
+}
---
+++ Algorítimos/ex022.js
@@ -0,0 +1,17 @@
+function farolRED() {
+    red.src = '/LIST-3/img/vermelho.png';
+    green.src = '/LIST-3/img/green2.png';
+    yellow.src ='/LIST-3/img/yellow2.png';
+}
+
+function farolYELLOW() {
+    yellow.src = "/LIST-3/img/amarelo.png"
+    green.src = "/LIST-3/img/green2.png"
+    red.src = "/LIST-3/img/red2.png"
+}
+
+function farolGREEN() {
+    green.src = "/LIST-3/img/verde.png"
+    yellow.src = "/LIST-3/img/yellow2.png"
+    red.src ="/LIST-3/img/red2.png"
+}
