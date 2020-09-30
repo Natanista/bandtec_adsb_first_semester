057dd03bae0fc7fcde5bc9659e63496361aa0c90
natanista <gdaynatan@gmail.com>
2020-09-30 12:01:18 -0300

    finalização de ajustes nos exercícios

--- Algorítimos/LIST-3/ex007.js
+++ Algorítimos/LIST-3/ex007.js
@@ -2,5 +2,6 @@ function calcular() {
     var faltasPorCem = Number(faltas.value) * 100;
     var divididoPorAulas =  faltasPorCem / Number(aulas.value);
     var frequencia = 100 - divididoPorAulas;
-    resultado.innerHTML = `Olá ${nome.value}! Sua frequência foi de:<div style="background-color: aqua ; height:20px; width:${frequencia}%" id="resultado">${frequencia}%</div>`;
+    resultado.innerHTML = `<span style='color:blue;'>Olá, ${nome.value}! Sua frequência foi de</span> <span style= 'color:green'><b>${frequencia}%</b></span>`;
+    div_barra.style.width = `${frequencia}%`;
 }
