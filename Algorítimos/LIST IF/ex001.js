ef76ca3eb847861de5c854712850b26d2b1ba60c
natanista <gdaynatan@gmail.com>
2020-10-02 09:09:22 -0300

    organização de pastas

--- Algorítimos/LIST IF/ex001.js
+++
@@ -1,8 +0,0 @@
- //Calcula a quantidade de refrigerantes
- function calcular() {
-    let consumo = Number(qtdRefri.value) * 365;
-    if (consumo >= 500) { //se >=500 exibe alert na tela
-        alerta.innerHTML = `Cuidado com a Diabetes!`;
-    }
-    resultado.innerHTML = `R: ${consumo}`;
-}
--- Algorítimos/LIST IF/ex003.js
+++
@@ -1,9 +0,0 @@
-let valorFinal = 0;
-
-        function calcular() {
-            valorFinal = (Number(preco.value)) / Number(amigos.value);
-            if(valorFinal > 20) {
-                alerta.innerHTML ="Deu Ruim!Pizza muito cara!";
-            }
-            resultado.innerHTML = `Cada amigo vai pagar R$${valorFinal.toFixed(2)} pela pizza`
-        }
