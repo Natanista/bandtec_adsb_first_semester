42a820fbf82540697c9d658f74bd847afcbf2d8f
natanista <gdaynatan@gmail.com>
2020-11-11 14:36:14 -0300

    exercicios de bolean

---
+++ Algorítimos/aulas/desigualdade.js
@@ -0,0 +1,8 @@
+let ax_verdadeiro = true;
+let ax_falso = false;
+ax_verdadeiro == 1 ? alert("true") : alert("false");
+ax_falso == 0 ? alert("false é comparavel com 0") : alert("false nao é comparavel com 0");
+ax_falso != 0 ? alert("nao pode ser") : alert("pode ser");
+ax_verdadeiro == true ? alert("posso comparar com true/false") : alert("nao posso comparar com true/false");
+ax_falso != true ? alert("posso comparar com desigualdade") : alert("erro");
+!ax_falso ? alert("posso negar o valor de uma variavel boleana") : alert("erro");
