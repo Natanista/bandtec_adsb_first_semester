55a8bd5cfc6fb45915842edf73e77c5f197faccb
natanista <gdaynatan@gmail.com>
2020-11-12 09:52:39 -0300

    continucao das atv

--- Algorítimos/LISTA 12 - For and Break/ex009.js
+++ Algorítimos/LISTA 12 - For and Break/ex009.js
@@ -1,11 +1,12 @@
 function ver(){
+    div_mens.innerHTML = ``;
     let quantidade = Number(document.getElementById("qtde").value);
 
-    for(let c = 1;c <= quantidade;c = c + 2,){
-        div_mens.innerHTML += `<img src="leite.png">`;
+    for(let c = 0;c <= quantidade;c = c + 2){
+        div_mens.innerHTML += `<img style="width:300px" src="img/leite.png">`;
     }
 
-    for(let l = 1; l <= quantidade; l++){
-        div_mens.innerHTML = ``;
+    for(let l = 0; l <= quantidade; l++){
+        div_mens.innerHTML += `<img style="width:300px" src="img/café.jpg">`;
     }
 }
